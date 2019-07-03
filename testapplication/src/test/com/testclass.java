28a927769bd4fa517e24c28de5a15cc390683f19
sureshak07 <suresh.ak@iinerds.com>
2019-07-03 18:13:56 +0530

    added new syso message

--- testapplication/src/test/com/testclass.java
+++ testapplication/src/test/com/testclass.java
@@ -3,7 +3,8 @@ package test.com;
 public class testclass {
 
 	public static void main(String[] args) {
-System.out.println("testing app");
+		System.out.println("testing app");
+		System.out.println("happy coding");
 	}
 
 }
